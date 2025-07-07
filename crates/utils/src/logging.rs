1d5f83affeb067a15a98701b85937111baf60549
Joey Yang <code2life@ustc.edu>
2025-07-07 17:50:50 +0800

    fix: add sync log to avoid debugging log missing issue

--- crates/utils/src/logging.rs
+++ crates/utils/src/logging.rs
@@ -2,10 +2,8 @@
 
 use std::env;
 use std::path::Path;
-use std::sync::OnceLock;
 
 use tracing::level_filters::LevelFilter;
-use tracing_appender::non_blocking::WorkerGuard;
 use tracing_appender::rolling::RollingFileAppender;
 use tracing_appender::rolling::Rotation;
 use tracing_subscriber::fmt::layer;
@@ -19,8 +17,6 @@ const ENABLE_LOG_ENV_VAR: &str = "TF_ENABLE_LOG";
 const LOG_PATH_ENV_VAR: &str = "TF_LOG_PATH";
 const LOG_LEVEL_ENV_VAR: &str = "TF_LOG_LEVEL";
 const LOG_OFF: &str = "off";
-
-static LOG_WORKER_GUARD: OnceLock<WorkerGuard> = OnceLock::new();
 
 /// initiate the global tracing subscriber
 pub fn get_fmt_layer() -> Box<dyn tracing_subscriber::Layer<Registry> + Send + Sync> {
@@ -51,15 +47,9 @@ pub fn get_fmt_layer() -> Box<dyn tracing_subscriber::Layer<Registry> + Send + S
                 .build(if is_dir { path } else { base_dir })
                 .expect("failed to create rolling file appender");
 
-            let (file_writer, guard) = tracing_appender::non_blocking(appender);
-
-            // keep non blocking write thread alive in global scope
-            LOG_WORKER_GUARD
-                .set(guard)
-                .expect("failed to set log worker guard");
-
+            // Use synchronous file writer directly
             layer()
-                .with_writer(file_writer)
+                .with_writer(appender)
                 .with_target(true)
                 .with_ansi(false)
                 .boxed()
