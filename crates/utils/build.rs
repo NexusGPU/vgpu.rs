3b1826154657b3681bef770af180674627358e77
0x5457 <0x5457@protonmail.com>
2025-12-19 20:29:37 +0800

    test

--- crates/utils/build.rs
+++ crates/utils/build.rs
@@ -4,6 +4,7 @@ fn main() {
     if let Err(err) = emit_git_variables() {
         println!("cargo:warning=vergen: {err}");
     }
+    
 
     let version = env::var("VERSION").unwrap_or_else(|_| "latest".to_string());
     println!("cargo:rustc-env=IMAGE_VERSION={version}");
